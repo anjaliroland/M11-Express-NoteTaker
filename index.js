1328ea48ba43caddf2d40673427cd852454f363c
Anjali Roland <anjaligroland@gmail.com>
2023-04-25 22:05:17 -0700

    added delete route

--- index.js
+++ index.js
@@ -10,36 +10,30 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-//html routes
 // GET Route for notes page
 app.get('/notes', (req, res) => 
-    // should return the notes.html file
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 // GET Route for homepage
-app.get('*', (req, res) => 
-    // should return the index.html file
+app.get('/', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/index.html'))
 )
 
-
-//api routes
 // GET Route for reading and returning saved notes
 app.get('/api/notes', (req, res) => {
-    // should read the db.json file and return all saved notes as JSON
-    fs.readFile("./db/db.json","utf-8",(err,data)=>{
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
         if(err){
-            return res.status(500).json({msg:"error reading db"})
+            return res.status(500).json({msg:'error reading db'})
         } else {
             const dataArr = JSON.parse(data);
             return res.json(dataArr)
         }
     })
 })
+
 // POST Route for adding new notes
 app.post('/api/notes', (req, res) => {
-    // should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
     fs.readFile('./db/db.json', 'utf-8', (err, data) => {
         if (err) {
             return res.status(500).json({msg: 'error reading db'})
@@ -64,9 +58,24 @@ app.post('/api/notes', (req, res) => {
 })
 
 
-// BONUS
+// DELETE Route to delete a note
 app.delete('/api/notes/:id', (req, res) => {
-    // should receive a query parameter that contains the id of a note to delete. To delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
+    const noteId = req.params.id;
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        if (err) {
+            return res.status(500).json({msg: 'error reading from db'})
+        } else {
+            const dataArr = JSON.parse(data);
+            const filteredData = dataArr.filter((note) => note.id !== noteId);
+            fs.writeFile('./db/db.json', JSON.stringify(filteredData, null, 4), (err) => {
+                if (err) {
+                    return res.status(500).json({msg: 'error deleting from db'})
+                } else {
+                    return res.json({msg: 'note deleted successfully'})
+                }
+            })
+        }
+    })
 })
 
 
